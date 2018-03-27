0962ce78f2c37354398ce46eef645c18fc136fa4
stand <2532258232@qq.com>
2018-03-27 17:58:12 +0800

    流量分析里面的小组件

---
+++ src/components/carBase/counter.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="counter-component">
+    <div class="counter-btn" @click="minus"> - </div>
+    <div class="counter-show">
+      <input type="text" v-model.number = "number" @keyup="restNumber">
+    </div>
+    <div class="counter-btn" @click="add"> + </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    max:{
+      type:Number,
+      default:15
+    },
+    min:{
+      type:Number,
+      default:1
+    }
+  },
+  data(){
+    return{
+      number:this.min
+    }
+  },
+  methods:{
+    minus(){
+      if(this.number <= this.min){
+        return
+      }
+      this.number--
+    },
+    add(){
+      if(this.number >= this.max){
+        return
+      }
+      this.number++
+    },
+    //输入数字处理
+    restNumber(){
+      let fix;
+      this.number = this.number.replace(/\D/g,'')
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .counter-component {
+    position: relative;
+    display: inline-block;
+    overflow: hidden;
+    vertical-align: middle;
+  }
+  .counter-show {
+    float: left;
+  }
+  .counter-show input {
+    border: none;
+    border-top: 1px solid #e3e3e3;
+    border-bottom: 1px solid #e3e3e3;
+    height: 25px;
+    line-height: 25px;
+    width: 40px;
+    outline: none;
+    text-align: center;
+  }
+  .counter-btn {
+    border: 1px solid #e3e3e3;
+    float: left;
+    height: 25px;
+    line-height: 25px;
+    width: 25px;
+    text-align: center;
+    cursor: pointer;
+  }
+  .counter-btn:hover {
+    border-color: #4fc08d;
+    background: #4fc08d;
+    color: #fff;
+  }
+
+</style>
--- src/pages/details/Analysis.vue
+++ src/pages/details/Analysis.vue
@@ -1,12 +1,75 @@
 <template>
-<div>
-  流量分析
-</div>
+  <div class="tong-ji">
+    <div class="detail-com-top">
+      <h3 class="detail-title">流量分析</h3>
+      <div class="title-explain">
+        <p>历史资料、科学实验、检验、统计等所获得的和用于科学研究、技术设计、查证、决策等的数值加以统计为解决方案做前期准备。</p>
+      </div>
+      <ul class="buy-details">
+        <li>
+          <span class="li-span">购买数量：</span>
+          <v-counter></v-counter>
+        </li>
+        <li>
+          <span class="li-span">产品类型：</span>
+          <select v-model="buyTypeSelect" @on-change="select()">
+            <option value="" disabled>请选择</option>
+            <option v-for="option in buyType" :value="option.value">{{option.text}}</option>
+          </select>
+          {{buyTypeSelect}}
+        </li>
+        <li><span class="li-span">有效时间：</span> <span>半年</span></li>
+        <li><span class="li-span">总价：</span><span>500元</span></li>
+        <li><span class="li-span"></span>
+          <a class="buy-btn"> 立即购买</a>
+        </li>
+
+      </ul>
+    </div>
+    <div class="detail-bottom">
+      <h3 class="detail-title">产品说明</h3>
+      <div class="title-des">
+        <p>des分析分析分析分析历史资料、科学实验、检验、统计等所获得的和用于科学研究、技术设计、查证、决策等的数值加以统计为解决方案做前期准备。</p>
+      </div>
+      <div class="goods-type">
+        电子电工  安工文件 礼品 生活服务
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
+  import vCounter from '@/components/carBase/counter'
+
     export default {
-        name: "analysis"
+      name: "analysis",
+      components: {vCounter},
+      data() {
+        return {
+          buyType: [
+            {text: '入门版', value: 0},
+            {text: '中级版', value: 1},
+            {text: '高级版', value: 2}
+          ],
+          buyTypeSelect: ''
+        }
+      },
+      computed: {
+        selects() {
+          // console.log(11111, this.buyTypeSelect);
+          return this.buyTypeSelect
+        }
+      },
+      methods: {
+        select() {
+          console.log(222, this.buyTypeSelect);
+          console.log(this.selects);
+        }
+      },
+      mounted(){
+        // this.select();
+      }
     }
 </script>
 
