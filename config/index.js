5ea80b2355ac0bd2d562ffb77a12b03f7ad88eaa
stand <2532258232@qq.com>
2018-05-23 17:29:24 +0800

    端口为8099

--- config/index.js
+++ config/index.js
@@ -11,12 +11,12 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api/':'http://localhost:8080/' //api就代表8081端口了
+      '/api/':'http://localhost:8099/' //api就代表8081端口了
     },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8099, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
